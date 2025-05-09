b2e8fdb9e07cef38269ff52b353099cd3f0a5147
Artyom <artyomgg07012012@gmail.com>
2025-05-09 12:20:54 +0300

    Update NineThOfMay.jsx

--- src/pages/NineThOfMay.jsx
+++ src/pages/NineThOfMay.jsx
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router'
+
 
 function NineThOfMay(props) {
 	const [timeLeft, setTimeLeft] = useState(calculateTimeLeft())
