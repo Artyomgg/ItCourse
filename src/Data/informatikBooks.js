f59dea2360bb66bd6267f81ce75360c3e8f038b2
unknown <artyomgg07012012@gmail.com  git config --global user.name>
2025-10-16 21:29:37 +0300

    fix

--- src/Data/informatikBooks.js
+++ src/Data/informatikBooks.js
@@ -1,8 +1,26 @@
 export const informaticsBooks = [
 	{
 		id: 1,
-		class: '7 класс',
+		class: '7 класс ОСНОВНЫЕ АЛГОРИТМИЧЕСКИЕ КОНСТРУКЦИИ',
 		title: 'Информатика 7 класс',
 		src: 'books/Учебник_Алгоритмизация_7_класс_af3e0ce1_1dd0_4e0f_8c99_2f398c28f6e3.pdf',
+	},
+	{
+		id: 2,
+		class: '6 класс',
+		title: 'Информатика 6 класс',
+		src: 'books/infarmatyka.06kl.2024.rus.pdf',
+	},
+	{
+		id: 3,
+		class: '7 класс',
+		title: 'Информатика 7 класс',
+		src: 'books/informatika-kotov-7kl-rus-2017 (1).pdf',
+	},
+	{
+		id: 4,
+		class: '8 класс',
+		title: 'Информатика 8 класс',
+		src: 'books/informatika_8kl_kotov_rus_2018 (1).pdf',
 	},
 ]
